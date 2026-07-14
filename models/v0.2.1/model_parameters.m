65c9cda8eaebb4169e76af61b29dff4ef8406fee
Korebami <100321918+Korebami@users.noreply.github.com>
2026-07-14 17:36:21 -0400

    Add files via upload
    Added supporting parameters file

---
+++ models/v0.2.1/model_parameters.m
@@ -0,0 +1,9 @@
+%For adaptive SOC algorithm -- NOT anymore
+Cdch= 1; % discharging rate in C-rate
+batt_temp = 273+25; % temperature of batt used in rul subsystem;
+max_SOC= 100; % MAXIMUM SOC in model simulation
+min_SOC= 43; % minimum SOC in model simulation
+batt_volt = 647; %total voltage of battery packs
+Crate = 360; %Battery capacity in Ahr)
+Cch=3; %Charging rate C-rate
+E_flight = 126; %energy of flight profile in kwhr for greenville to augusta
